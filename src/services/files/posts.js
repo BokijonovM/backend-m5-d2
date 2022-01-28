e3e65b6e2a85902b88fe1171c95c7b78901cc570
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-28 08:13:48 +0100

    working

--- src/services/files/posts.js
+++ src/services/files/posts.js
@@ -21,7 +21,7 @@ export const uploadFile = (req, res, next) => {
     const fileName = `${req.params.postId}${extension}`;
     const pathToFile = path.join(publicDirectory, fileName);
     fs.writeFileSync(pathToFile, buffer);
-    const link = `http://localhost:3001/${fileName}`;
+    const link = `http://localhost:3001/public/img/posts/${fileName}`;
     req.file = link;
     next();
   } catch (error) {
